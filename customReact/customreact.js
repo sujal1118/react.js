a399c9b361abb7af3416d3d3a7bfc93053fb6930
sujal1118 <your@email.com~>
2025-07-09 17:05:00 +0530

    add loo code

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,24 @@
 function customElement(reactElement, container){
-    const domElement = document.createElemen
+   /*
+    const domElement = document.createElement
     (reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(domElement)
+    domElement.innerHTML = reactElement.props.children;
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+
+    container.appendChild(domElement);
+    */
+
+    // loop base code
+    const domElement = document.creatElement
+    (reactElement.type);
+    domElement.innerHTML = reactElement.props.children;
+    for(const prop in reactElement.props){
+        if(prop == 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+
+    }
+    container.appendchild(domElement);
 }
 
 const reactElement = {
@@ -12,10 +26,10 @@ const reactElement = {
     props :
     {
         href: 'https://google.com',
-        terget : '_blank',
+        target : '_blank',
         children: "click me to visit google"
     }
-}
+};
 
 const mainContainer = document.querySelector('#root')
-customReact(reactElement, mainContainer)   
+customElement(reactElement, mainContainer)   
