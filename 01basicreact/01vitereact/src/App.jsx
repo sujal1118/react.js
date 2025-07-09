f950e1e69f6d2ecc1deb6b6858fc4f1e801f9d17
sujal1118 <your@email.com~>
2025-07-09 18:43:48 +0530

    add new function

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,15 +1,15 @@
 import Vite from "./vite"
 
 function App() {
+  const username = "chai aur code";
 
   return (
     <div>
      <Vite/>
         <p> test para</p>
-     <h1>jsx must wrapeed in an enclosing tag</h1>
+     <h1>jsx must wrapeed in an enclosing tag{username}</h1>
   
      </div>
   )
 }
-
 export default App
--- 01basicreact/01vitereact/src/main.jsx
+++ 01basicreact/01vitereact/src/main.jsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+ 
+function MyApp(){
+    return (
+        <div>
+            <h1> custom app | vite</h1>
+        </div>
+    )
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// const ReactElement = {
+//     type : 'a', 
+//     props :
+//     {
+//         href: 'https://google.com',
+//         target : '_blank',
+//     },
+//         children: "click me to visit google"
+    
+// };
+
+// const anotherElement = (
+//     <a href="https://google.com" target="_blank">
+//         click me to visit google
+//     </a>
+// );
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'click visit google'
+);
+
+const container = document.getElementById('root');
+ReactDOM.createRoot(container).render(ReactElement); // ✅ this works if ReactDOM is globally defined
+
+
